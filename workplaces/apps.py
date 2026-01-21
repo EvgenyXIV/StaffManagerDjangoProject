3e53b95eeb3e7ea50670aa58ab76b873076d1440
Evgeny K. <evgeny.kruchinin@gmail.com>
2026-01-21 18:20:28 +0300

    Delete workplaces directory

--- workplaces/apps.py
+++
@@ -1,7 +0,0 @@
-from django.apps import AppConfig
-
-
-class WorkplacesConfig(AppConfig):
-    default_auto_field = "django.db.models.BigAutoField"
-    name = "workplaces"
-    verbose_name = "РАБОЧИЕ МЕСТА"
